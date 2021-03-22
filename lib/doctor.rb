c61ae0ba759e7f0e869cc6d2c9bf3285b97e7e12
Mariah Morales <mjustine622@gmail.com>
2021-03-22 10:12:12 -0400

    commit tree 70be8f259c9e111045ca55c1ccd4bda0a6e4fb04

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,44 @@
 
+class Doctor
+    #rspec spec/04_doctor_spec.rb
+    attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def appointments
+    find_patient
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def find_patient
+    Appointment.all.select do |appt|
+      appt.doctor == self
+    end
+  end
+
+  def patients
+    patients = []
+
+    find_patient.each do |patient|
+      patients << patient.patient
+    end
+    patients
+  end
+
+  def self.all
+    @@all
+  end
+
+end
